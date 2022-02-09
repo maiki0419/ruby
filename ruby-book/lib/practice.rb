4d9353c7f32d434d210053e50cdcac575aeb794d
maiki0419 <95464965+maiki0419@users.noreply.github.com>
2022-02-09 21:45:35 +0900

    ruby-book-2/9-21

--- ruby-book/lib/practice.rb
+++ ruby-book/lib/practice.rb
@@ -1,9 +1,33 @@
 
-def a(x,y,c = "japan")
-  puts x
-  puts y
-  puts c
-
+def a
+  array = [1,2,3,4]
+  sample = []
+  array.each{|n| sample<< n*10}
+  sample.each{|m| m}
 end
 
-a("japan","ss")
+def b
+  array = [1,2,3,4]
+  sample = array.map{|n| n*10}
+  sample.each{|m| m}
+end
+
+def c
+  array = [1,2,3,4]
+  sample = array.select{|n| n.even?}
+  sample.each{|m| m}
+end
+
+
+def d
+  array = [1,2,3,4]
+  sample = array.reject{|n| n.even?}
+  sample.each{|m| m}
+end
+
+def e
+  array = [1,2,3,4]
+  array.find{|n| n.even?}
+end
+
+puts e()
